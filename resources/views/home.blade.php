edcf8d26a53a6b71dff191ff4f58766c8cee08f2
karitelena <capozzer@gmail.com>
2020-04-17 02:29:38 -0300

    commit inicial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('template.template')
+
+@section('title', 'Home')
+@section('content')
+
+<div class="home">
+    
+</div>
+@endsection
---
+++ resources/views/photos.blade.php
@@ -0,0 +1,39 @@
+@extends('template.template')
+
+@section('title', 'Photos')
+@section('content')
+
+<!-- modal -->
+<div class="modal">
+    <div class="modal_content">
+        <img src="{{ asset('/img/aniverfelipe.JPG') }}" id="modal_img">
+    </div>
+    <span id="bt_close">&times;</span>
+</div>
+    <!-- galeria -->
+<div class="gallery">
+    <div class="row">
+        <div class="card_img">
+            <img src="{{ asset('/img/aniverfelipe.JPG') }}" class="small_img">
+        </div>
+        <div class="card_img">
+            <img src="{{ asset('/img/cutura.JPG') }}" class="small_img">
+        </div>
+        <div class="card_img">
+            <img src="{{ asset('/img/meuquarto.JPG') }}" class="small_img">
+        </div>
+    </div>
+    <div class="row">
+        <div class="card_img">
+            <img src="{{ asset('/img/feira.JPG') }}" class="small_img">
+        </div>
+        <div class="card_img">
+            <img src="{{ asset('/img/quartolaura.JPG') }}" class="small_img">
+        </div>
+        <div class="card_img">
+            <img src="{{ asset('/img/tilapia.JPG') }}" class="small_img">
+        </div>
+    </div>        
+</div>
+<script src="{{ asset('js/galeria.js') }}"></script>
+@endsection
